bc739f789e0760b44472835ad8e5713edf180264
Christopher Carrillo <carrillocrespo17@gmail.com>
2025-05-15 22:59:55 +0200

    ControllerBattleAttack acabado y Optimizado

--- projecte/src/main/java/com/projecte/Pokemon.java
+++ projecte/src/main/java/com/projecte/Pokemon.java
@@ -3,6 +3,9 @@ package com.projecte;
 public class Pokemon {
     private final String name;
     private final String imagePath;
+    private int currentHP;
+    private int currentStamina;
+    private boolean defeated = false;
 
     public Pokemon(String name, String imagePath) {
         this.name = name;
@@ -17,4 +20,27 @@ public class Pokemon {
         return imagePath;
     }
 
+    public int getCurrentHP() {
+        return currentHP;
+    }
+
+    public void setCurrentHP(int currentHP) {
+        this.currentHP = currentHP;
+    }
+
+    public int getCurrentStamina() {
+        return currentStamina;
+    }
+
+    public void setCurrentStamina(int currentStamina) {
+        this.currentStamina = currentStamina;
+    }
+
+    public boolean isDefeated() { 
+        return defeated; 
+    }
+
+    public void setDefeated(boolean defeated) { 
+        this.defeated = defeated; 
+    }
 }
